d62d319e2d26fbf04c13f100c1cca9c0ea0ca41a
yaroslav <garasym@gmail.com>
2016-12-08 11:01:21 +0200

    Formatting code

--- src/lesson01/Main.java
+++ src/lesson01/Main.java
@@ -5,7 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		int number = ConsoleHelper.getNaturalNumber();
 		// 86
-		ConsoleHelper.writeMessage("Кількість цифр в числі: " + MyMath.getCountOfDigitsInNaturalNumber(number));
+		ConsoleHelper.writeMessage("Кількість цифр в числі: "
+				+ MyMath.getCountOfDigitsInNaturalNumber(number));
 		ConsoleHelper.writeMessage("Перша цифра в числі: " + MyMath.getFirstDigitInNaturalNumber(number));
 		ConsoleHelper.writeMessage("Сума цифр в числі: " + MyMath.getSumOfDigitsInNaturalNumber(number));
 		ConsoleHelper.writeMessage("Знакозмінна сума цифр в числі: " + MyMath.getAlternatingSumOfDigitsInNaturalNumber(number));		
