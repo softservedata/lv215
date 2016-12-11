92575e26eb2044fbb05668330bcc0178a3afdf57
“OleksandrButyter“ <olekbut@gmail.com>
2016-12-12 00:48:01 +0200

    Add butyter.task01.LogicalTasks

--- src/butyter/task01/Task01.java
+++ src/butyter/task01/Task01.java
@@ -2,4 +2,10 @@ package butyter.task01;
 
 public class Task01 {
 
+	public static void main(String[] args) {
+		int number = LogicalTasks.inputNaturalNumber();
+		System.out.println("Reversed number (Task 88b) - " + LogicalTasks.reverseNumber(number));
+		System.out.println("Mersenne primes (Task 559) - " + LogicalTasks.getMersennePrimes(number));
+		System.out.println("Natural dividers (Task 224) - " + LogicalTasks.getNaturalDividers(number));
+	}
 }
