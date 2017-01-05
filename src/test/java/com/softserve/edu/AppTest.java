7b0a0b893c833ba8f9fda4ffdd8cd43d8b7431eb
yaroslav <garasym@gmail.com>
2017-01-05 11:22:22 +0200

    template maven project

---
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -0,0 +1,18 @@
+package com.softserve.edu;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void testApp() {
+        System.out.println("+++ surefire.reports.directory = " 
+                + System.getProperty("surefire.reports.directory"));
+        Assert.assertTrue(true);
+    }
+
+}
