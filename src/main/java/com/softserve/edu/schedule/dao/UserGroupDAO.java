3cb118121c6703b579351602bf8e0c282afbe9f3
Andrew Zhidenko <andrewzhidenko@gmail.com>
2017-01-17 15:59:05 +0200

    Added UserGroups dao and services

---
+++ src/main/java/com/softserve/edu/schedule/dao/UserGroupDAO.java
@@ -0,0 +1,21 @@
+/*
+ * UserGroupDAO.java
+ * 1.0
+ * 09 Jan 2017
+ * Copyright (c) Zhydenko Andrii
+ */
+package com.softserve.edu.schedule.dao;
+
+import com.softserve.edu.schedule.entity.UserGroup;
+
+/**
+ * A simple DAO interface to handle the database operation required to
+ * manipulate additional info in UserGroup.
+ *
+ * @version 1.0 09 Jan 2017
+ * @author Zhydenko Andrii
+ *
+ */
+public interface UserGroupDAO extends CrudDAO<UserGroup> {
+
+}
---
+++ src/main/java/com/softserve/edu/schedule/service/implementation/UserGroupServiceImpl.java
@@ -0,0 +1,68 @@
+/*
+ * UserGroupServiceImpl.java
+ * 1.0
+ * 09 Jan 2017
+ * Copyright (c) Zhydenko Andrii
+ */
+package com.softserve.edu.schedule.service.implementation;
+
+import java.util.List;
+
+import com.softserve.edu.schedule.dao.Order;
+import com.softserve.edu.schedule.dao.UserGroupDAO;
+import com.softserve.edu.schedule.entity.UserGroup;
+import com.softserve.edu.schedule.service.UserGroupService;
+
+/**
+ * A UserGroupService implementation to handle the operation required to
+ * manipulate a UserGroup menu.
+ *
+ * @version 1.0 09 Jan 2017
+ * @author Zhydenko Andrii
+ *
+ */
+public class UserGroupServiceImpl implements UserGroupService {
+	UserGroupDAO userGroupDAO;
+
+	@Override
+	public void create(UserGroup userGroup) {
+		userGroupDAO.create(userGroup);
+	}
+
+	@Override
+	public void update(UserGroup userGroup) {
+		userGroupDAO.update(userGroup);
+	}
+
+	@Override
+	public UserGroup getById(Long id) {
+		return userGroupDAO.getById(id);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		UserGroup group = userGroupDAO.getById(id);
+		userGroupDAO.delete(group);
+	}
+
+	@Override
+	public List<UserGroup> getAll() {
+		return userGroupDAO.getAll();
+	}
+
+	@Override
+	public void delete(UserGroup userGroup) {
+		userGroupDAO.delete(userGroup);
+	}
+
+	@Override
+	public List<UserGroup> sort(String field, Order order) {
+		return userGroupDAO.sort(field, order);
+	}
+
+	@Override
+	public List<UserGroup> search(String field, String pattern) {
+		return userGroupDAO.search(field, pattern);
+	}
+
+}
