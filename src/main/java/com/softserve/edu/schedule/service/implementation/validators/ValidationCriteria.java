1494cdbbec455abd37f3c2cd9dabbe9cc48b6c60
Zed180881 <zed180881@gmail.com>
2017-01-20 18:28:02 +0200

    added interfaces for validation constants

---
+++ src/main/java/com/softserve/edu/schedule/service/implementation/validators/ValidationCriteria.java
@@ -0,0 +1,25 @@
+/* ValidationCriteria 1.0 01/20/2017 */
+package com.softserve.edu.schedule.service.implementation.validators;
+
+/**
+ * An interface to storage validation criteria regular expressions.
+ *
+ * @version 1.0 20 January 2017
+ *
+ * @author Petro Zelyonka
+ *
+ * @since 1.8
+ */
+public interface ValidationCriteria {
+
+    /**
+     * Allowed characters for name expression.
+     */
+    String CHARACTERS_FOR_NAME = "^[а-яА-ЯёЁїЇa-zA-Z0-9\\-№ ]+$";
+
+    /**
+     * Allowed only digits expression.
+     */
+    String DIGITS_ONLY = "[0-9]+";
+
+}
