300e34159fa6b82881ce74f3c0e0b124ba409edb
Zed180881 <zed180881@gmail.com>
2017-01-25 22:41:49 +0200

    implements aspect

---
+++ src/main/java/com/softserve/edu/schedule/aspect/DeleteEventsAspect.java
@@ -0,0 +1,55 @@
+package com.softserve.edu.schedule.aspect;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.softserve.edu.schedule.dto.MeetingCompactDTO;
+import com.softserve.edu.schedule.entity.MeetingStatus;
+import com.softserve.edu.schedule.entity.Room;
+import com.softserve.edu.schedule.service.implementation.dtoconverter.MeetingCompactDTOConverter;
+import com.softserve.edu.schedule.service.implementation.mailsenders.MeetingCanceledMailService;
+
+@Aspect
+public class DeleteEventsAspect {
+
+    /**
+     * RoomDTOConverter example to provide to DTO and from DTO conversion.
+     */
+    @Autowired
+    private MeetingCompactDTOConverter meetingCompactDTOConverter;
+
+    /**
+     * MeetingCanceledMailService example to provide send mail to user
+     * operations.
+     */
+    @Autowired
+    private MeetingCanceledMailService meetingCanceledMailService;
+
+    /**
+     * Set to all meetings in room null value in field room, changed status of
+     * all future meetings to NOT_APPROVED and send mail messages to their
+     * owners.
+     * 
+     * @param room
+     *            Room to proceed meetings
+     * 
+     */
+    @Before("execution(* com.softserve.edu.schedule.dao.implementation.RoomDAOImpl.delete(..)) && args(room,..)")
+    public void processingRoomBeforeDeletion(Room room) {
+        List<MeetingCompactDTO> meetingToAlert = new ArrayList<>();
+        room.getMeetings().forEach(e -> {
+            if (e.getDate().isAfter(LocalDate.now().minusDays(1))) {
+                e.setStatus(MeetingStatus.NOT_APPROVED);
+                meetingToAlert.add(meetingCompactDTOConverter.getDTO(e));
+            }
+            e.setRoom(null);
+        });
+        meetingToAlert.forEach(
+                e -> meetingCanceledMailService.sendInfoMessageRoomDeletion(e));
+    }
+}
