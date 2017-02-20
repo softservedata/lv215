298c5d99d0823c8ff95616af5e52bcaa6b1d2b4c
anarkn <bohdmelnyk@gmail.com>
2017-02-20 15:52:05 +0200

    Add meetingHistoryService

--- src/main/java/com/softserve/edu/schedule/cronotasks/MeetingHistoryCreator.java
+++ src/main/java/com/softserve/edu/schedule/cronotasks/MeetingHistoryCreator.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.softserve.edu.schedule.aspects.PerfomanceLoggable;
+
 import com.softserve.edu.schedule.dao.MeetingDAO;
 import com.softserve.edu.schedule.dao.MeetingHistoryDAO;
 import com.softserve.edu.schedule.entity.Meeting;
