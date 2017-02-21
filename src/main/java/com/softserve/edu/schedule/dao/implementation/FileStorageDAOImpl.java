473076f3801eab443197c2b8167ade2ac0407c8e
rhinocrones <rhinocrones@gmail.com>
2017-02-21 14:38:17 +0200

    mongodb config

---
+++ src/main/java/com/softserve/edu/schedule/dao/implementation/FileStorageDAOImpl.java
@@ -0,0 +1,53 @@
+package com.softserve.edu.schedule.dao.implementation;
+
+import java.io.InputStream;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.mongodb.DBObject;
+import com.mongodb.gridfs.GridFSDBFile;
+import com.softserve.edu.schedule.dao.FileStorageDAO;
+
+@Repository
+public class FileStorageDAOImpl implements FileStorageDAO {
+
+	@Autowired
+	private GridFsTemplate gridFsTemplate;
+
+	public String store(InputStream inputStream, String fileName,
+	        String contentType, DBObject metaData) {
+		return this.gridFsTemplate
+		        .store(inputStream, fileName, contentType, metaData).getId()
+		        .toString();
+	}
+
+	public GridFSDBFile getById(String id) {
+		return this.gridFsTemplate
+		        .findOne(new Query(Criteria.where("_id").is(id)));
+	}
+
+	public GridFSDBFile getByFilename(String fileName) {
+		return gridFsTemplate
+		        .findOne(new Query(Criteria.where("filename").is(fileName)));
+	}
+
+	public GridFSDBFile retrive(String fileName) {
+		return gridFsTemplate
+		        .findOne(new Query(Criteria.where("filename").is(fileName)));
+	}
+
+	public List<GridFSDBFile> findAll() {
+		return gridFsTemplate.find(null);
+	}
+
+	@Override
+	public void delete(String id) {
+		gridFsTemplate.delete(new Query(Criteria.where("_id").is(id)));
+	}
+
+}
